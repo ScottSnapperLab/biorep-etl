3e447086c873248983ca2b8feffd82b63bffa429
Gus Dunn <w.gus.dunn@gmail.com>
2017-01-06 11:46:08 -0500

    Initial attempts are mixed...

---
+++ src/biorep_etl/data/field_definitions/func_slug.py
@@ -0,0 +1,464 @@
+def record_id(df, dd):
+    """Return True if the record_id column tests pass."""
+    pass
+
+def samplenumber(df, dd):
+    """Return True if the samplenumber column tests pass."""
+    pass
+
+def label_on_sample(df, dd):
+    """Return True if the label_on_sample column tests pass."""
+    pass
+
+def mrn(df, dd):
+    """Return True if the mrn column tests pass."""
+    pass
+
+def biorepidnumber(df, dd):
+    """Return True if the biorepidnumber column tests pass."""
+    pass
+
+def completed_by(df, dd):
+    """Return True if the completed_by column tests pass."""
+    pass
+
+def date_crf_completed(df, dd):
+    """Return True if the date_crf_completed column tests pass."""
+    pass
+
+def sample_date(df, dd):
+    """Return True if the sample_date column tests pass."""
+    pass
+
+def registryconsent(df, dd):
+    """Return True if the registryconsent column tests pass."""
+    pass
+
+def biorepconsent(df, dd):
+    """Return True if the biorepconsent column tests pass."""
+    pass
+
+def prior_protocol_number(df, dd):
+    """Return True if the prior_protocol_number column tests pass."""
+    pass
+
+def ibdyesno(df, dd):
+    """Return True if the ibdyesno column tests pass."""
+    pass
+
+def ibdtype(df, dd):
+    """Return True if the ibdtype column tests pass."""
+    pass
+
+def otheribddx(df, dd):
+    """Return True if the otheribddx column tests pass."""
+    pass
+
+def family_history(df, dd):
+    """Return True if the family_history column tests pass."""
+    pass
+
+def familymember_ibd(df, dd):
+    """Return True if the familymember_ibd column tests pass."""
+    pass
+
+def other_relative(df, dd):
+    """Return True if the other_relative column tests pass."""
+    pass
+
+def ibd_immunodeficiencies(df, dd):
+    """Return True if the ibd_immunodeficiencies column tests pass."""
+    pass
+
+def other_immunodeficiency(df, dd):
+    """Return True if the other_immunodeficiency column tests pass."""
+    pass
+
+def controldx(df, dd):
+    """Return True if the controldx column tests pass."""
+    pass
+
+def other_diagnosis(df, dd):
+    """Return True if the other_diagnosis column tests pass."""
+    pass
+
+def control_immunodeficiency(df, dd):
+    """Return True if the control_immunodeficiency column tests pass."""
+    pass
+
+def otherimmuno_control(df, dd):
+    """Return True if the otherimmuno_control column tests pass."""
+    pass
+
+def psc(df, dd):
+    """Return True if the psc column tests pass."""
+    pass
+
+def familymembertype(df, dd):
+    """Return True if the familymembertype column tests pass."""
+    pass
+
+def familymembergidx(df, dd):
+    """Return True if the familymembergidx column tests pass."""
+    pass
+
+def family_otherdiagnosis(df, dd):
+    """Return True if the family_otherdiagnosis column tests pass."""
+    pass
+
+def gender(df, dd):
+    """Return True if the gender column tests pass."""
+    pass
+
+def dob(df, dd):
+    """Return True if the dob column tests pass."""
+    pass
+
+def age(df, dd):
+    """Return True if the age column tests pass."""
+    pass
+
+def neopics_sample(df, dd):
+    """Return True if the neopics_sample column tests pass."""
+    pass
+
+def chb_id(df, dd):
+    """Return True if the chb_id column tests pass."""
+    pass
+
+def neopics(df, dd):
+    """Return True if the neopics column tests pass."""
+    pass
+
+def symptom_onset(df, dd):
+    """Return True if the symptom_onset column tests pass."""
+    pass
+
+def date_of_diagnosis(df, dd):
+    """Return True if the date_of_diagnosis column tests pass."""
+    pass
+
+def consanguinity(df, dd):
+    """Return True if the consanguinity column tests pass."""
+    pass
+
+def oral_perianal(df, dd):
+    """Return True if the oral_perianal column tests pass."""
+    pass
+
+def nsaid(df, dd):
+    """Return True if the nsaid column tests pass."""
+    pass
+
+def nsaid_doses(df, dd):
+    """Return True if the nsaid_doses column tests pass."""
+    pass
+
+def sampletype(df, dd):
+    """Return True if the sampletype column tests pass."""
+    pass
+
+def other_sampletype(df, dd):
+    """Return True if the other_sampletype column tests pass."""
+    pass
+
+def longitudinal(df, dd):
+    """Return True if the longitudinal column tests pass."""
+    pass
+
+def blood_samples(df, dd):
+    """Return True if the blood_samples column tests pass."""
+    pass
+
+def timetominus20(df, dd):
+    """Return True if the timetominus20 column tests pass."""
+    pass
+
+def timetominus80(df, dd):
+    """Return True if the timetominus80 column tests pass."""
+    pass
+
+def stoolmedia(df, dd):
+    """Return True if the stoolmedia column tests pass."""
+    pass
+
+def stool_dna(df, dd):
+    """Return True if the stool_dna column tests pass."""
+    pass
+
+def bxlocation(df, dd):
+    """Return True if the bxlocation column tests pass."""
+    pass
+
+def surgicallocations(df, dd):
+    """Return True if the surgicallocations column tests pass."""
+    pass
+
+def where_stored(df, dd):
+    """Return True if the where_stored column tests pass."""
+    pass
+
+def other_shipped(df, dd):
+    """Return True if the other_shipped column tests pass."""
+    pass
+
+def labmember(df, dd):
+    """Return True if the labmember column tests pass."""
+    pass
+
+def hbi_wellbeing(df, dd):
+    """Return True if the hbi_wellbeing column tests pass."""
+    pass
+
+def hbi_abdpain(df, dd):
+    """Return True if the hbi_abdpain column tests pass."""
+    pass
+
+def hbi_liquidstools(df, dd):
+    """Return True if the hbi_liquidstools column tests pass."""
+    pass
+
+def hbi_abdominalmass(df, dd):
+    """Return True if the hbi_abdominalmass column tests pass."""
+    pass
+
+def hbi_complications(df, dd):
+    """Return True if the hbi_complications column tests pass."""
+    pass
+
+def hbi_abscess(df, dd):
+    """Return True if the hbi_abscess column tests pass."""
+    pass
+
+def hbi_fissure(df, dd):
+    """Return True if the hbi_fissure column tests pass."""
+    pass
+
+def hbi_aphthous(df, dd):
+    """Return True if the hbi_aphthous column tests pass."""
+    pass
+
+def hbi_arthralgia(df, dd):
+    """Return True if the hbi_arthralgia column tests pass."""
+    pass
+
+def hbi_nodosum(df, dd):
+    """Return True if the hbi_nodosum column tests pass."""
+    pass
+
+def hbi_fistula(df, dd):
+    """Return True if the hbi_fistula column tests pass."""
+    pass
+
+def hbi_gangrenosum(df, dd):
+    """Return True if the hbi_gangrenosum column tests pass."""
+    pass
+
+def hbi_uveitis(df, dd):
+    """Return True if the hbi_uveitis column tests pass."""
+    pass
+
+def hbi_total(df, dd):
+    """Return True if the hbi_total column tests pass."""
+    pass
+
+def sccai_stoolsperday(df, dd):
+    """Return True if the sccai_stoolsperday column tests pass."""
+    pass
+
+def sccai_stoolspernight(df, dd):
+    """Return True if the sccai_stoolspernight column tests pass."""
+    pass
+
+def sccai_urgency(df, dd):
+    """Return True if the sccai_urgency column tests pass."""
+    pass
+
+def sccai_blood(df, dd):
+    """Return True if the sccai_blood column tests pass."""
+    pass
+
+def sccai_wellbeing(df, dd):
+    """Return True if the sccai_wellbeing column tests pass."""
+    pass
+
+def sccai_eim(df, dd):
+    """Return True if the sccai_eim column tests pass."""
+    pass
+
+def sccai_arthralgia(df, dd):
+    """Return True if the sccai_arthralgia column tests pass."""
+    pass
+
+def sccai_nodosum(df, dd):
+    """Return True if the sccai_nodosum column tests pass."""
+    pass
+
+def sccai_gangrenosum(df, dd):
+    """Return True if the sccai_gangrenosum column tests pass."""
+    pass
+
+def sccai_uveitis(df, dd):
+    """Return True if the sccai_uveitis column tests pass."""
+    pass
+
+def sccai_total(df, dd):
+    """Return True if the sccai_total column tests pass."""
+    pass
+
+def pucai_abdpain(df, dd):
+    """Return True if the pucai_abdpain column tests pass."""
+    pass
+
+def pucai_bleeding(df, dd):
+    """Return True if the pucai_bleeding column tests pass."""
+    pass
+
+def pucai_consistency(df, dd):
+    """Return True if the pucai_consistency column tests pass."""
+    pass
+
+def pucai_frequency(df, dd):
+    """Return True if the pucai_frequency column tests pass."""
+    pass
+
+def pucai_nocturnal(df, dd):
+    """Return True if the pucai_nocturnal column tests pass."""
+    pass
+
+def pucai_limitation(df, dd):
+    """Return True if the pucai_limitation column tests pass."""
+    pass
+
+def pucai_total(df, dd):
+    """Return True if the pucai_total column tests pass."""
+    pass
+
+def anymeds(df, dd):
+    """Return True if the anymeds column tests pass."""
+    pass
+
+def current_5asa(df, dd):
+    """Return True if the current_5asa column tests pass."""
+    pass
+
+def asa_type(df, dd):
+    """Return True if the asa_type column tests pass."""
+    pass
+
+def current_antibiotic(df, dd):
+    """Return True if the current_antibiotic column tests pass."""
+    pass
+
+def antibiotic_type(df, dd):
+    """Return True if the antibiotic_type column tests pass."""
+    pass
+
+def other_antibiotic(df, dd):
+    """Return True if the other_antibiotic column tests pass."""
+    pass
+
+def current_probiotic(df, dd):
+    """Return True if the current_probiotic column tests pass."""
+    pass
+
+def probiotic_type(df, dd):
+    """Return True if the probiotic_type column tests pass."""
+    pass
+
+def other_probiotic(df, dd):
+    """Return True if the other_probiotic column tests pass."""
+    pass
+
+def current_glucocorticoid(df, dd):
+    """Return True if the current_glucocorticoid column tests pass."""
+    pass
+
+def glucocorticoid_type(df, dd):
+    """Return True if the glucocorticoid_type column tests pass."""
+    pass
+
+def current_immunomodulator(df, dd):
+    """Return True if the current_immunomodulator column tests pass."""
+    pass
+
+def immunomodulator_type(df, dd):
+    """Return True if the immunomodulator_type column tests pass."""
+    pass
+
+def current_antitnf(df, dd):
+    """Return True if the current_antitnf column tests pass."""
+    pass
+
+def antitnf_type(df, dd):
+    """Return True if the antitnf_type column tests pass."""
+    pass
+
+def current_calcineurin(df, dd):
+    """Return True if the current_calcineurin column tests pass."""
+    pass
+
+def calcineurin_type(df, dd):
+    """Return True if the calcineurin_type column tests pass."""
+    pass
+
+def current_otherbiologics(df, dd):
+    """Return True if the current_otherbiologics column tests pass."""
+    pass
+
+def otherbiologic_type(df, dd):
+    """Return True if the otherbiologic_type column tests pass."""
+    pass
+
+def fecaltransplant(df, dd):
+    """Return True if the fecaltransplant column tests pass."""
+    pass
+
+def current_othermeds(df, dd):
+    """Return True if the current_othermeds column tests pass."""
+    pass
+
+def othermed_type(df, dd):
+    """Return True if the othermed_type column tests pass."""
+    pass
+
+def othermed(df, dd):
+    """Return True if the othermed column tests pass."""
+    pass
+
+def date_labs(df, dd):
+    """Return True if the date_labs column tests pass."""
+    pass
+
+def hct(df, dd):
+    """Return True if the hct column tests pass."""
+    pass
+
+def wbc(df, dd):
+    """Return True if the wbc column tests pass."""
+    pass
+
+def plt(df, dd):
+    """Return True if the plt column tests pass."""
+    pass
+
+def crp(df, dd):
+    """Return True if the crp column tests pass."""
+    pass
+
+def esr(df, dd):
+    """Return True if the esr column tests pass."""
+    pass
+
+def alb(df, dd):
+    """Return True if the alb column tests pass."""
+    pass
+
+def notes(df, dd):
+    """Return True if the notes column tests pass."""
+    pass
+
+def ibd_biorepository_sample_database_complete(df, dd):
+    """Return True if the ibd_biorepository_sample_database_complete column tests pass."""
+    pass
+
--- src/biorep_etl/data/load_recode.py
+++ src/biorep_etl/data/load_recode.py
@@ -12,6 +12,11 @@ import numpy as np
 
 from munch import Munch
 
+import engarde.decorators as ed
+import engarde.checks as ck
+
+
+import biorep_etl.data.field_definitions.redcap_dump as rcd
 import biorep_etl.errors as e
 
 # Metadata
@@ -20,14 +25,47 @@ __email__ = "w.gus.dunn@gmail.com"
 
 
 # functions
-def init_load_data_and_labels(data_, labels_):
+def make_field_map(data_dict):
+    """Return Dict to translate field names and field labels."""
+    d = Munch()
+    field_names = data_dict.reset_index()['Variable / Field Name'].values
+    field_labels = data_dict['Field Label'].values
+    
+    d = Munch({k:v for k,v in zip(field_names, field_labels)})
+    d.update(Munch({d:l for d,l in zip(field_labels, field_names)}))
+    
+    return d
+
+def load_data_dict(data_dict_):
+    """Load data dict into df."""
+    return pd.read_csv(data_dict_, index_col=0)
+
+
+
+def load_redcap_dump(data_):
+    """Return loaded, recode, and validated dump table."""
+    dtypes = {'samplenumber': np.object,
+              'biorepidnumber': np.object,}
+    
+    return pd.read_csv(data_, dtype=dtypes, index_col=0)
+    
+
+
+def init_load_data_and_labels(data_, data_dict_):
+    """Return Munch obj containing the loaded and verified data dump and the field_map."""
     loaded = Munch()
     
-    loaded.data = pd.read_csv(data_)
-    labels = pd.read_csv(labels_)
+    data_dict = load_data_dict(data_dict_=data_dict_)
+    required = rcd.req_cols(dd=data_dict)
     
-    loaded.conv_labels = Munch({d:l for d,l in zip(loaded.data.columns.values, labels.columns.values)})
-    loaded.conv_labels.update(Munch({d:l for d,l in zip(labels.columns.values, loaded.data.columns.values)}))
+    loaded.data = load_redcap_dump(data_=data_).pipe(ck.none_missing,
+                                                     columns=required)
+    
+    
+    loaded.field_map = make_field_map(data_dict=data_dict)
+    
+    
+
     
     return loaded
     
