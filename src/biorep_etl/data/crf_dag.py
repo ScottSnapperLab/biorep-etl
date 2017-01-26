bd2191cf627ad6d20aa3d3a7f0a3d22de94cac62
Gus Dunn <w.gus.dunn@gmail.com>
2017-01-26 11:23:11 -0500

    HREG branching logic, and choice_defintion parsers

--- src/biorep_etl/data/crf_dag.py
+++ src/biorep_etl/data/crf_dag.py
@@ -11,13 +11,20 @@ import networkx as nx
 
 from munch import Munch
 
+from biorep_etl.data.parsers import BranchLogicParser
 import biorep_etl.errors as e
 
 # Metadata
 __author__ = "Gus Dunn"
 __email__ = "w.gus.dunn@gmail.com"
 
+
+# Constants
 EdgeLabel = namedtuple(typename='EdgeLabel', field_names=["n1", "n2", "label"], verbose=False, rename=False)
+
+# StartNodeAndLabel = namedtuple(typename, field_names, verbose=False, rename=False)
+
+# branch_logic_parser = build_branch_logic_parser()
 
 # Functions
 
@@ -41,46 +48,73 @@ def make_top_level_nodes_and_others(edges_raw):
     
     return  d
 
-def parse_branch_logic(logic):
-    """Return `n1`, `label` based on a single branch logic string."""
-    # try:
-    #     n1, label = logic.replace("'",'"').replace('[','').replace(']','').replace('"',"").replace(' = ',"=").split('=')
-    # except ValueError:
-    #     n1, label = logic.replace("'",'"').replace('[','').replace(']','').replace('"',"").replace(' = ',"=").split('=')
-    
-    n1, label = logic.replace("'",'"').replace('[','').replace(']','').replace('"',"").replace(' = ',"=").split('=')
-    
-    if '(' in n1:
-        n1, label = n1.replace(')','').split('(')
-        
-    return n1, label
+# def parse_branch_logic_old(logic):
+#     """Return `n1`, `label` based on a single branch logic string."""
+#     # try:
+#     #     n1, label = logic.replace("'",'"').replace('[','').replace(']','').replace('"',"").replace(' = ',"=").split('=')
+#     # except ValueError:
+#     #     n1, label = logic.replace("'",'"').replace('[','').replace(']','').replace('"',"").replace(' = ',"=").split('=')
+#
+#     n1, label = logic.replace("'",'"').replace('[','').replace(']','').replace('"',"").replace(' = ',"=").split('=')
+#
+#     if '(' in n1:
+#         n1, label = n1.replace(')','').split('(')
+#
+#     return n1, label
     
 
+
+def parse_branch_logic(logic, n2):
+    """Yield ``EdgeLabel`` namedtuples derived from each branch comparison encoded in ``logic``.
+    
+    logic (str): the redcap table-row's branch_logic column string.
+    n2 (str): the redcap table-row's field_name.
+    
+    Yields:
+        EdgeLabel: namedtuples ("n1", "n2", "label")
+    """
+    parser = BranchLogicParser()
+    parser.parse_string(string=logic)
+    
+    for v in parser.variables:
+        yield EdgeLabel(":".join(str(i) for i in v), n2, "") # TODO: add actual labels
+
+# def make_lower_edges_old(lower_edges_raw):
+#     """Yield ``EdgeLabel`` namedtuples ("n1", "n2", "label") for the lower edges based on branch logic."""
+#     field_name, branch_logic = 0,1
+#
+#     for row in lower_edges_raw.values:
+#
+#         try:
+#             n1, label = parse_branch_logic(logic=row[branch_logic])
+#             n2 = row[field_name]
+#             yield EdgeLabel(n1=n1,n2=n2,label=label)
+#
+#         except ValueError as exc:
+#             if ' or ' in row[branch_logic]:
+#                 logics = [l for l in row[branch_logic].split(' or ')]
+#                 n2 = row[field_name]
+#
+#                 for parsed_logic in [parse_branch_logic(logic=l) for l in logics]:
+#                     n1, label = parsed_logic
+#                     yield EdgeLabel(n1=n1,n2=n2,label=label)
+            
 def make_lower_edges(lower_edges_raw):
-    """Yield named tuples ("n1", "n2", "label") based on branch logic."""
+    """Yield ``EdgeLabel`` namedtuples ("n1", "n2", "label") for the lower edges based on branch logic."""
     field_name, branch_logic = 0,1
     
     for row in lower_edges_raw.values:
         
-        try:
-            n1, label = parse_branch_logic(logic=row[branch_logic])
-            n2 = row[field_name]
-            yield EdgeLabel(n1=n1,n2=n2,label=label)
+        yield parse_branch_logic(logic=row[branch_logic], n2=row[field_name])
+        
             
-        except ValueError as exc:
-            if ' or ' in row[branch_logic]:
-                logics = [l for l in row[branch_logic].split(' or ')]
-                n2 = row[field_name]
-                
-                for parsed_logic in [parse_branch_logic(logic=l) for l in logics]:
-                    n1, label = parsed_logic
-                    yield EdgeLabel(n1=n1,n2=n2,label=label)
-            
+        
 
 def add_lower_edges(g,lower_edges_raw):
     """Add lower level nodes with edge labels."""
-    for e in make_lower_edges(lower_edges_raw):
-        g.add_edge(u=e.n1, v=e.n2, label=e.label)
+    for lower_edges in make_lower_edges(lower_edges_raw):
+        for e in lower_edges:
+            g.add_edge(u=e.n1, v=e.n2, label=e.label)
         
     
 def add_top_level_edges(g,top_level_nodes):
