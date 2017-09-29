45011ecff43bd2001238f14b93a0a9b7829acc21
Gus Dunn <w.gus.dunn@gmail.com>
2017-09-29 12:52:43 -0400

    data.validation: added `is_in` and `not_in`

--- src/biorep_etl/data/validation.py
+++ src/biorep_etl/data/validation.py
@@ -5,3 +5,17 @@
 
 
 # Functions
+def is_in(x, y):
+    """Return ``True`` if ``x`` is in ``y``, ``False`` otherwise."""
+    if x in y:
+        return True
+    else:
+        return False
+
+
+def not_in(x, y):
+    """Return ``True`` if ``x`` is NOT in ``y``, ``False`` otherwise."""
+    if x not in y:
+        return True
+    else:
+        return False
